fd80a0d141d6a520d622dcf3dc1688b578c3ce50
linuxknow <linuxknow@gmail.com>
2014-08-16 19:41:58 -0300

    Instalación modificacion

--- instalar.sh
+++ instalar.sh
@@ -2,11 +2,19 @@
 
 if [ $UID -eq 0 ];then
  echo "Proceeding"
- install --mode=755 * /opt/atnot3s/
- ln -s /opt/atnot3s/execAnot3s /usr/bin/execAnot3s
- install --mode=755 anot3s.desktop /usr/share/applications/
- gxmessage "En hora buena usted instalo atnot3e GPL"
+ mkdir -p /opt/atnot3s/
+ cp -r * /opt/atnot3s/
+ chmod -R 755 /opt/atnot3s/
+ if [ ! -e /usr/bin/execAnot3s ];then
+  ln -s /opt/atnot3s/execAnot3s /usr/bin/execAnot3s 2>/dev/null 2>&1
+ fi
+ if [ -x /usr/bin/gxmessage ];then
+   install --mode=755 anot3s.desktop /usr/share/applications/
+   gxmessage "En hora buena usted instalo atnot3e GPL"
+ else
+   echo "Debe instalar gxmessage: apt-get install gxmessage"
+ fi
 else
-    gxmessage "Requiere permisos de root para instalar"
+   echo "Requiere permisos de root para instalar"
 fi
 
